f9bf03edc3944bc69dc64c531d400c1b87df20c5
lucasSabatino99 <lucassabatino@outlook.com>
2021-02-22 22:07:23 -0300

    Alteracao 1

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from '../auth/token';
 
 const api = axios.create({
-  baseURL: 'https://apprenddy.herokuapp.com/',
+  baseURL: 'http://localhost:3333',
 });
 
 api.interceptors.request.use(async config => {
